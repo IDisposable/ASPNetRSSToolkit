28dcb84751bd384a194b8b51f481a1dbe97af98a
IDisposable_cp <SND\IDisposable_cp>
2007-05-04 08:34:41 +0000

    Better structure for exception handling.

--- v2/RssDl/Rssdl.cs
+++ v2/RssDl/Rssdl.cs
@@ -12,7 +12,7 @@ namespace RssToolkit
     /// <summary>
     /// Generates code files based on a Rss or Opml URL
     /// </summary>
-    public sealed class Rssdl 
+    public sealed class Rssdl
     {
         private Rssdl()
         {
@@ -23,13 +23,13 @@ namespace RssToolkit
         /// </summary>
         /// <param name="args">Arguments.</param>
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2201:DoNotRaiseReservedExceptionTypes"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:DoNotPassLiteralsAsLocalizedParameters", MessageId = "System.Exception.#ctor(System.String)")]
-        public static void Main(string[] args) 
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Simple RSS Compiler v1.0");
+            Console.WriteLine("Simple RSS Compiler v2.0");
             Console.WriteLine();
 
             // Process command line
-            if (args.Length != 2) 
+            if (args.Length != 2)
             {
                 Console.WriteLine("usage: rssdl.exe url-or-file outputcode.cs");
                 return;
@@ -41,64 +41,57 @@ namespace RssToolkit
 
             // Load the channel data from supplied url
             string codeString;
-            try 
+            try
             {
                 using (Stream feedStream = DownloadManager.GetFeed(url))
                 {
                     using (XmlTextReader reader = new XmlTextReader(feedStream))
                     {
                         codeString = RssXmlHelper.ConvertToRssXml(reader);
+
+                        try
+                        {
+                            // Open the output code file
+                            using (TextWriter codeWriter = new StreamWriter(codeFilename, false))
+                            {
+                                // Get the language from file extension
+                                string lang = Path.GetExtension(codeFilename);
+
+                                if (lang != null && lang.Length > 1 && lang.StartsWith("."))
+                                {
+                                    lang = lang.Substring(1).ToUpperInvariant();
+                                }
+                                else
+                                {
+                                    lang = "CS";
+                                }
+
+                                // Generate source
+                                try
+                                {
+                                    RssCodeGenerator.GenerateCode(codeString, url, lang, string.Empty, classNamePrefix, codeWriter, true);
+                                    Console.WriteLine("Done -- generated '{0}'.", codeFilename);
+                                    return;
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine("*** Error generating '{0}' *** {1}: {2}", codeFilename, e.GetType().Name, e.Message);
+                                }
+                            }
+
+                            File.Delete(codeFilename);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("*** Failed to open '{0}' for writing *** {1}: {2}", codeFilename, e.GetType().Name, e.Message);
+                        }
                     }
                 }
             }
-            catch (Exception e) 
-            {
-                Console.WriteLine("*** Failed to load '{0}' *** {1}: {2}", url, e.GetType().Name, e.Message);
-                return;
-            }
-
-            // Open the output code file
-            TextWriter codeWriter;
-            try
-            {
-                codeWriter = new StreamWriter(codeFilename, false);
-            }
-            catch (Exception e) 
-            {
-                Console.WriteLine("*** Failed to open '{0}' for writing *** {1}: {2}", codeFilename, e.GetType().Name, e.Message);
-                return;
-            }
-
-            // Get the language from file extension
-            string lang = Path.GetExtension(codeFilename);
-
-            if (lang != null && lang.Length > 1 && lang.StartsWith("."))             
-            {
-                lang = lang.Substring(1).ToUpperInvariant();
-            }
-            else 
-            {
-                lang = "CS";
-            }
-
-            // Generate source
-            try 
-            {
-                RssCodeGenerator.GenerateCode(codeString, url, lang, string.Empty, classNamePrefix, codeWriter, true);
-            }
             catch (Exception e)
             {
-                codeWriter.Dispose();
-                File.Delete(codeFilename);
-
-                Console.WriteLine("*** Error generating '{0}' *** {1}: {2}", codeFilename, e.GetType().Name, e.Message);
-                return;
+                Console.WriteLine("*** Failed to load '{0}' *** {1}: {2}", url, e.GetType().Name, e.Message);
             }
-
-            // Done
-            codeWriter.Dispose();
-
-            Console.WriteLine("Done -- generated '{0}'.", codeFilename);
         }
     }
 }
